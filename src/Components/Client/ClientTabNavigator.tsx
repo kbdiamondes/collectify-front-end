7d9f56e212ca39add6419084f66508c6efe6ce1f
kbdiamondes <kbdiamantes@gmail.com>
2023-05-20 22:25:03 +0800

    Tab Name Fix v2
    Changes made:
    1. Title property is set to Short words
    2. HeaderTitle property is set to the Complete Word

--- src/Components/Client/ClientTabNavigator.tsx
+++ src/Components/Client/ClientTabNavigator.tsx
@@ -11,11 +11,11 @@ const Tab = createBottomTabNavigator()
 export function TabNavigator() {
   return (
         <Tab.Navigator screenOptions={{headerShown:true, headerTitleAlign: 'center'}}>
-           <Tab.Screen name='Due Payments' component={DuePayments} options={{title: 'Dues',  tabBarInactiveBackgroundColor: '#0A1C34', tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="calendar" color='#fff' size={20}></Ionicons>)}}/>
-           <Tab.Screen name='Scheduled Payments' component={SchedulePayments} options={{title: 'Payments', tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="time" color='#fff' size={20}></Ionicons>)}}/>
-           <Tab.Screen name='Payment Reminders' component={PaymentReminders} options={{title: 'Reminders', tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="chatbubbles" color='#fff' size={20}></Ionicons>)}}/>
-           <Tab.Screen name='Transaction History' component={TransactionHistory} options={{title: 'Transactions', tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="list" color='#fff' size={20}></Ionicons>)}}/>
-           <Tab.Screen name='Payment Records' component={PaymentRecords} options={{title: 'Records', tabBarAllowFontScaling: true, tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="card" color='#fff' size={20}></Ionicons>)}}/>
+           <Tab.Screen name='Due Payments' component={DuePayments} options={{headerTitle: 'Payment Dues',title: 'Dues',  tabBarInactiveBackgroundColor: '#0A1C34', tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="calendar" color='#fff' size={20}></Ionicons>)}}/>
+           <Tab.Screen name='Scheduled Payments' component={SchedulePayments} options={{headerTitle: 'Scheduled Payment', title: 'Payments', tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="time" color='#fff' size={20}></Ionicons>)}}/>
+           <Tab.Screen name='Payment Reminders' component={PaymentReminders} options={{headerTitle: 'Payment Reminders', title: 'Reminders', tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="chatbubbles" color='#fff' size={20}></Ionicons>)}}/>
+           <Tab.Screen name='Transaction History' component={TransactionHistory} options={{headerTitle:'Transaction History', title: 'Transactions', tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="list" color='#fff' size={20}></Ionicons>)}}/>
+           <Tab.Screen name='Payment Records' component={PaymentRecords} options={{headerTitle: 'Payment Records', title: 'Records', tabBarAllowFontScaling: true, tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="card" color='#fff' size={20}></Ionicons>)}}/>
         </Tab.Navigator>
    )
 }
