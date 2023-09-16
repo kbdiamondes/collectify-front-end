22d47a9d47509eef6b9ab8eebca749b963cce775
Keith <kbdiamantes@gmail.com>
2023-09-16 17:17:20 +0800

    Register Screen Responsive
    Registration Screen now responsive

--- src/Components/Register.tsx
+++ src/Components/Register.tsx
@@ -3,31 +3,132 @@ import React from "react";
 import { SafeAreaView, Text, View, StyleSheet, Pressable, TextInput} from "react-native";
 import { CheckScreenNavigationprop } from "../../App";
 
-export default function Login(){
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+export default function Register(){
     const navigation = useNavigation<CheckScreenNavigationprop>(); 
 
     return(
-        <SafeAreaView>
-            <View style={styles.container}>
-                
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>    
                 <Text style={styles.textStyleSubheader} onPress={()=>navigation.goBack()}>Back</Text>
                 <Text style={styles.textStyleHeader}>Get Started</Text>
-                
-                <TextInput placeholderTextColor="#C2C6CC" style={styles.textBoxStyle} placeholder="Enter username" ></TextInput>
-                <TextInput placeholderTextColor="#C2C6CC" style={styles.textBoxStyle} placeholder="Enter password" ></TextInput>
-                <TextInput placeholderTextColor="#C2C6CC" style={styles.textBoxStyle} placeholder="Full Name" ></TextInput>
-                <TextInput placeholderTextColor="#C2C6CC" style={styles.textBoxStyle} placeholder="Email Address" ></TextInput>
-                <View style={styles.buttonContainer}>
-                    <Pressable style={styles.button} onPressIn={()=>alert("Registered'")}>
-                        <Text style={styles.buttonLabel}>Register</Text>
-                    </Pressable>
-                </View>                
             </View>
+
+            <View style={styles.main}>
+                <View style={styles.body}>
+                    <TextInput placeholderTextColor="#C2C6CC" style={styles.textBoxStyle} placeholder="Enter username" ></TextInput>
+                    <TextInput placeholderTextColor="#C2C6CC" style={styles.textBoxStyle} placeholder="Enter password" ></TextInput>
+                    <TextInput placeholderTextColor="#C2C6CC" style={styles.textBoxStyle} placeholder="Full Name" ></TextInput>
+                    <TextInput placeholderTextColor="#C2C6CC" style={styles.textBoxStyle} placeholder="Email Address" ></TextInput>
+                    
+                    <View style={styles.button}>
+                        <Pressable onPressIn={()=>alert("Registered'")}>
+                            <Text style={styles.buttonLabel}>Register</Text>
+                        </Pressable>
+                    </View>               
+                </View>
+            </View> 
+            
         </SafeAreaView>
     );
 
 }
 
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        height: hp(100)
+    },
+    header:{
+        flex: 0.5, 
+        height:hp(50), 
+        margin: hp(5)
+    }, 
+    main:{
+        flex: 2.5,
+        display: 'flex', 
+        flexDirection: 'row', 
+    }, 
+    body:{
+        flex: 1, 
+        width: wp(100), 
+        paddingLeft: hp(5), 
+        paddingRight: hp(5)
+    }, 
+    footer:{
+        flex:0.3, 
+        height: hp(50), 
+        paddingLeft: hp(5)
+    }, 
+    textStyleSubheader:{
+        paddingTop: 35, 
+        paddingBottom: 25
+    },
+    textStyleHeader:{
+        fontSize: hp(5), 
+        fontWeight: 'bold', 
+    },
+
+    textStyleSmallest:{
+        fontSize: hp(1.5),
+    },
+
+    textSmallestContainer:{ 
+        flex:1,
+        marginTop: hp(2), 
+        marginBottom: hp(1), 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+
+
+    },  
+    textBoxStyle:{
+        height: hp(7), 
+        paddingLeft: wp(5),
+        marginBottom: hp(1.5), 
+        borderColor: '#F0F2F4', 
+        borderWidth: 2,
+        color:'#363636', 
+
+    },
+
+    button:{
+
+        backgroundColor: '#2C85E7',
+        height: hp(7),
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        borderRadius: 5, 
+    },
+    buttonLabel:{
+        color: '#fff', 
+        fontSize: hp(2)
+    },
+
+    buttonUnfilled:{
+        borderRadius: 5,
+        borderWidth: 2, 
+        borderColor: '#F0F2F4',
+        height: hp(7), 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        flexDirection: 'row', 
+        backgroundColor: '#fff', 
+        marginTop: '5%'
+    },
+
+    buttonUnfilledLabel:{
+        color: '#4A5B6B', 
+        fontSize: hp(1.2)
+    }, 
+
+});
+
+
+/*
 const styles = StyleSheet.create({
     container: {
         justifyContent: 'flex-start', 
@@ -113,3 +214,5 @@ const styles = StyleSheet.create({
         color: '#4A5B6B', 
     }
 });
+
+*/
