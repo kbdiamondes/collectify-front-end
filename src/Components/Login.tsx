d62865c741451f08a5891c621781e645cf350eb8
Keith <kbdiamantes@gmail.com>
2023-09-18 22:29:31 +0800

    Responsive Tell us About Yourself Screen
    Made the Tell us about yourself screen dynamic

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -28,7 +28,7 @@ export default function Login(){
             <View style={styles.main}>
                 <View style={styles.body}>
                     <TextInput placeholderTextColor="#C2C6CC" style={styles.textBoxStyle} placeholder="Enter username" ></TextInput>
-                    <TextInput placeholderTextColor="#C2C6CC" style={styles.textBoxStyle} placeholder="Enter password" ></TextInput>
+                    <TextInput placeholderTextColor="#C2C6CC" style={styles.textBoxStyle} placeholder="Enter password" secureTextEntry={true}></TextInput>
 
                     <View  style={styles.button}>
                         <Pressable onPressIn={()=>navigation.push('TellUsMoreAboutYourself')}>
@@ -136,90 +136,3 @@ const styles = StyleSheet.create({
 
 });
 
-
-
-/*
-const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'flex-start', 
-        alignItem: 'flext-start', 
-        paddingHorizontal: 35,
-        paddingVertical: 80
-    }, 
-    textStyleSubheader:{
-        paddingTop: 35, 
-        paddingBottom: 25
-    },
-    textStyleHeader:{
-        fontSize: 55, 
-        fontWeight: 'bold', 
-    },
-
-    textStyleSmallest:{
-        fontSize: 13,
-        alignItems:'center',
-        justifyContent:'center', 
-        marginTop: 16,
-        marginBottom:16, 
-        paddingLeft:115
-    },
-
-    textBoxStyle:{
-        width: 320, 
-        height: 50, 
-        paddingLeft: 20,
-        borderColor: '#F0F2F4', 
-        borderWidth: 2,
-        marginBottom: 13, 
-        color:'#363636', 
-    },
-
-    buttonContainer:{
-        width: 318, 
-        height: 48, 
-        backgroundColor: '#2C85E7',
-        alignItems: 'center', 
-        justifyContent: 'center', 
-        borderRadius: 5, 
-        marginTop: 30
-    }, 
-
-    button:{
-        borderRadius: 10 ,
-        width: '100%', 
-        height: '100%', 
-        alignItems: 'center', 
-        justifyContent: 'center', 
-        flexDirection: 'row'
-    },
-
-    buttonLabel:{
-        color: '#fff', 
-    },
-
-    buttonUnfilledContainer:{
-        width: 318, 
-        height: 48, 
-        backgroundColor: '#fff',
-        alignItems: 'center', 
-        justifyContent: 'center', 
-        borderRadius: 5, 
-        borderColor: '#F0F2F4',
-        borderWidth: 2, 
-        marginTop: 30
-    }, 
-
-    buttonUnfilled:{
-        borderRadius: 10 ,
-        width: '100%', 
-        height: '100%', 
-        alignItems: 'center', 
-        justifyContent: 'center', 
-        flexDirection: 'row'
-    },
-
-    buttonUnfilledLabel:{
-        color: '#4A5B6B', 
-    }, 
-
-});*/
