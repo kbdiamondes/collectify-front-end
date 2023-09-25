82eceeb4b018374475aa767a0230e187c93e7998
Keith <kbdiamantes@gmail.com>
2023-09-25 10:46:32 +0800

    Bottom-Tab-Navigation-Redesigned

--- src/Components/Client/ClientTabNavigator.tsx
+++ src/Components/Client/ClientTabNavigator.tsx
@@ -8,18 +8,44 @@ import PaymentRecords from "./PaymentRecords";
 
 
 import {SafeAreaView, View, StyleSheet, Platform} from 'react-native'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 
 const Tab = createBottomTabNavigator()
 
+/*
+tabBarOptions={{
+style: {
+backgroundColor: 'red',
+borderTopLeftRadius: 25,
+borderTopRightRadius: 25,
+position: "absolute",
+bottom: 0,
+},
+}}
+
+*/
 export function TabNavigator() {
   return (
-      <View style={styles.TabNavStyle}>
-        <Tab.Navigator screenOptions={{headerShown:true, headerTitleAlign: 'center'}}>
-           <Tab.Screen name='Due Payments' component={DuePayments} options={{headerTitle: 'Payment Dues',title: 'Dues',  tabBarInactiveBackgroundColor: '#0A1C34', tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="calendar" color='#fff' size={20}></Ionicons>)}}/>
-           <Tab.Screen name='Scheduled Payments' component={SchedulePayments} options={{headerTitle: 'Scheduled Payment', title: 'Payments', tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="time" color='#fff' size={20}></Ionicons>)}}/>
-           <Tab.Screen name='Payment Reminders' component={PaymentReminders} options={{headerTitle: 'Payment Reminders', title: 'Reminders', tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="chatbubbles" color='#fff' size={20}></Ionicons>)}}/>
-           <Tab.Screen name='Transaction History' component={TransactionHistory} options={{headerTitle:'Transaction History', title: 'Transactions', tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="list" color='#fff' size={20}></Ionicons>)}}/>
-           <Tab.Screen name='Payment Records' component={PaymentRecords} options={{headerTitle: 'Payment Records', title: 'Records', tabBarAllowFontScaling: true, tabBarInactiveBackgroundColor: '#0A1C34',  tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="card" color='#fff' size={20}></Ionicons>)}}/>
+      <View style={styles.TabNavStyle} >
+        <Tab.Navigator screenOptions={{headerShown:true, 
+            headerTitleAlign: 'center', 
+            tabBarInactiveBackgroundColor: '#0A1C34',
+            tabBarInactiveTintColor: '#A7ACB2',
+            tabBarActiveTintColor: '#fff',
+            tabBarStyle: {backgroundColor:'rgb(10,28,52)',
+            flex:.065,
+            borderTopLeftRadius: hp(2),
+            borderTopRightRadius: hp(2), 
+            borderLeftWidth:hp(.5), 
+            borderRightWidth: hp(.5), 
+            position: 'relative', 
+            overflow: 'hidden'}}}>
+           <Tab.Screen name='Due Payments' component={DuePayments} options={{  headerTitle: 'Payment Dues',title: 'Dues', tabBarIcon: (focus)=>(<Ionicons name="calendar" color='#fff'size={20}></Ionicons>)}}/>
+           <Tab.Screen name='Scheduled Payments' component={SchedulePayments} options={{headerTitle: 'Scheduled Payment', title: 'Payments', tabBarIcon: (focus)=>(<Ionicons name="time" color='#fff' size={20}></Ionicons>)}}/>
+           <Tab.Screen name='Payment Reminders' component={PaymentReminders} options={{headerTitle: 'Payment Reminders', title: 'Reminders', tabBarIcon: (focus)=>(<Ionicons name="chatbubbles" color='#fff' size={20}></Ionicons>)}}/>
+           <Tab.Screen name='Transaction History' component={TransactionHistory} options={{headerTitle:'Transaction History', title: 'Transactions', tabBarIcon: (focus)=>(<Ionicons name="list" color='#fff' size={20}></Ionicons>)}}/>
+           <Tab.Screen name='Payment Records' component={PaymentRecords} options={{headerTitle: 'Payment Records', title: 'Records', tabBarIcon: (focus)=>(<Ionicons name="card" color='#fff' size={20}></Ionicons>)}}/>
         </Tab.Navigator>
       </View>
    )
@@ -29,5 +55,5 @@ const styles = StyleSheet.create({
    TabNavStyle:{
       flex:1,
       justifyContent: Platform.OS === 'ios' ? 'space-between' : 'flex-end',
-   }
+   },
 });
--- src/Components/Reseller/ResellerTabNavigator.tsx
+++ src/Components/Reseller/ResellerTabNavigator.tsx
@@ -4,15 +4,40 @@ import ResellerSoldItems from "./ResellerSoldItems";
 import MyCollectors from "./MyCollectors";
 import CollectorCollection from "./CollectorCollection";
 
+import {SafeAreaView, View, StyleSheet, Platform} from 'react-native'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 const Tab = createBottomTabNavigator()
 
 export function ResellerTabNavigator() {
   return (
-        <Tab.Navigator screenOptions={{headerShown:true, headerTitleAlign: 'center'}}>
-           <Tab.Screen name='ItemsSold' component={ResellerSoldItems} options={{headerTitle: 'Sold Items',title: 'Items Sold',  tabBarInactiveBackgroundColor: '#0A1C34', tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="clipboard" color='#fff' size={20}></Ionicons>)}}/>
-           <Tab.Screen name='MyCollector' component={MyCollectors} options={{headerTitle: 'My Collectors',title: 'My Collectors',  tabBarInactiveBackgroundColor: '#0A1C34', tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="people" color='#fff' size={20}></Ionicons>)}}/>
-           <Tab.Screen name='CollectorCollection' component={CollectorCollection} options={{headerTitle: 'Collector Collections',title: 'Collector Collections',  tabBarInactiveBackgroundColor: '#0A1C34', tabBarActiveTintColor: '#fff', tabBarIcon: ()=>(<Ionicons name="send" color='#fff' size={20}></Ionicons>)}}/>          
+      <View style={styles.TabNavStyle}>
+         
+         <Tab.Navigator screenOptions={{headerShown:true, 
+            headerTitleAlign: 'center', 
+            tabBarInactiveBackgroundColor: '#0A1C34',
+            tabBarInactiveTintColor: '#A7ACB2',
+            tabBarActiveTintColor: '#fff',
+            tabBarStyle: {backgroundColor:'rgb(10,28,52)',
+            flex:.065,
+            borderTopLeftRadius: hp(2),
+            borderTopRightRadius: hp(2), 
+            borderLeftWidth:hp(.5), 
+            borderRightWidth: hp(.5), 
+            position: 'relative', 
+            overflow: 'hidden'}}}>
+           <Tab.Screen name='ItemsSold' component={ResellerSoldItems} options={{headerTitle: 'Sold Items',title: 'Items Sold', tabBarIcon: ()=>(<Ionicons name="clipboard" color='#fff' size={20}></Ionicons>)}}/>
+           <Tab.Screen name='MyCollector' component={MyCollectors} options={{headerTitle: 'My Collectors',title: 'My Collectors', tabBarIcon: ()=>(<Ionicons name="people" color='#fff' size={20}></Ionicons>)}}/>
+           <Tab.Screen name='CollectorCollection' component={CollectorCollection} options={{headerTitle: 'Collector Collections',title: 'Collector Collections', tabBarIcon: ()=>(<Ionicons name="send" color='#fff' size={20}></Ionicons>)}}/>          
         </Tab.Navigator>
+      </View>
    )
 }
+
+
+const styles = StyleSheet.create({
+   TabNavStyle:{
+      flex:1,
+      justifyContent: Platform.OS === 'ios' ? 'space-between' : 'flex-end',
+   },
+});
