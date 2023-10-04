d4612a4b602386d83a449de3f0d9e626149dd199
bilbaw <114888495+bilbaw@users.noreply.github.com>
2023-10-04 19:10:37 +0800

    axios and confirm success
    done

--- src/Components/Client/PaymentForm.tsx
+++ src/Components/Client/PaymentForm.tsx
@@ -1,17 +1,40 @@
-import {SafeAreaView, View, Text, StyleSheet, ScrollView, TextInput, Pressable} from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet, ScrollView, TextInput, Pressable, Modal} from 'react-native';
 import React, { useState } from 'react';
 import CameraCapture from './Camera';
 import { useNavigation } from "@react-navigation/native";
 import axios from 'axios';
 import { CheckScreenNavigationprop } from '../../../App';
 import { Picker } from '@react-native-picker/picker';
-
+import {Ionicons} from '@expo/vector-icons'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 export default function PaymentForm(){
+    const [itemName, setitemName] = useState('')
     const [itemPrice, setitemPrice] = useState(0)
+    const [requiredCollectible, setrequiredCollectible] = useState(0)
     const [referenceNumber, setreferenceNumber] = useState(0)
     const [paymentType, setpaymentType] = useState('')
     const [transactionProof, settransactionProof] = useState<any>(null)
+
+    const [isModalVisible, setIsModalVisible] = useState(false)
+    const handleModal = () => setIsModalVisible(()=>!isModalVisible)
+
+     //checks passed data from console
+     const continueButton = () => {
+        console.log(itemName);
+        console.log(itemPrice);
+        console.log(requiredCollectible);
+        console.log(referenceNumber);
+        console.log(paymentType);
+        console.log(transactionProof);
+        handleModal() //shows the modal
+    }
+
+    const confirmContract = () =>{
+        alert("Success")
+        handleModal() //hides the modal
+        //pass value here
+    }
 
     const navigation  = useNavigation<CheckScreenNavigationprop>();
 
@@ -19,6 +42,7 @@ export default function PaymentForm(){
       })
       .then(function (response) {
         console.log(response);
+        handleModal()
       })
       .catch(function (error) {
         console.log(error);
@@ -39,8 +63,27 @@ export default function PaymentForm(){
     }
    
     return(
+//modal
+        
+
 
         <SafeAreaView>
+            <Modal animationType="slide" transparent={true} visible={isModalVisible}>
+                <View style={{justifyContent: 'center', alignItems: 'center', flex:1, backgroundColor: 'rgba(0, 0, 0, 0.5)'}}>
+
+                    <View style={styles.modalView}>
+
+                        <Ionicons name="warning-sharp" color="grey" size={hp(12)}></Ionicons>
+                        <Text style={{fontSize: hp(2.5)}}>Confirm Purchase?</Text>
+                        <Text style={{fontSize: hp(1.2), fontWeight: '300', flexWrap: 'wrap', padding: hp(1.2)}}>Are you sure about this purchase?</Text>
+                        <View style={styles.modalButtonConfirmation}>
+                            <Pressable onPressIn={confirmContract}>
+                                <Text style={{fontSize: hp(2), fontWeight: 'bold', color: '#fff'}}>Confirm</Text>
+                            </Pressable>
+                        </View>         
+                    </View>
+                </View>
+            </Modal>
             <ScrollView>
             <View style={styles.container}>
                 <Text style={styles.textHeader} >Pay Dues</Text>
@@ -68,8 +111,15 @@ export default function PaymentForm(){
                             Take picture
                         </Text>
                         </Pressable>
+                     
                     </View>
-
+                    <View style={styles.buttonContainer}>
+                    <Pressable style={styles.button} onPressIn={handleSubmit}>
+                        <Text style={styles.buttonLabel}>
+                            Continue
+                        </Text>
+                        </Pressable>
+                    </View>
                 </View>
             </View>    
             </ScrollView>     
@@ -140,10 +190,36 @@ const styles = StyleSheet.create({
         height: 40,
         borderRadius: 5,
         width: '50%'
-    }
+    },
+    modalButtonConfirmation:{
+        marginTop: hp(2), 
+        backgroundColor: '#2C85E7',
+        width: wp(35),
+        height: hp(6.5),
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        borderRadius: 5, 
+    }, 
+    modalView: {
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        margin: hp(5),
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: hp(5),
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
 
-    
 });
+    
+
 
 
 /*
