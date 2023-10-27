f547f4d2a43dd61b6bb9d1e0c69501b22401fe2e
Keith <kbdiamantes@gmail.com>
2023-10-28 00:41:20 +0800

    coldevening_dev1028v1240am
    # Added
    
    ## Screen
    
    ### My Collectors
    - Assigned Collectors can now be viewed on the "My Collectors" tab

--- src/Components/Reseller/MyCollectors.tsx
+++ src/Components/Reseller/MyCollectors.tsx
@@ -7,80 +7,49 @@ import MyCollectorList from './Lists/MyCollectorList';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { AuthContext } from '../../Context/AuthContext';
 import { BASE_URL } from '../../../config';
-import { RestAPI } from '../../Services/RestAPI';
+import { ICollector, RestAPI } from '../../Services/RestAPI';
+import axios from 'axios';
 
-const assignedCollectors = [
-    {
-        personName: 'John Doe', 
-        itemCollectible: 2500, 
-        collectionStatus: 'Collecting' 
-    },
-    {
-        personName: 'John Doe', 
-        itemCollectible: 2500, 
-        collectionStatus: 'Collecting' 
-    },
-    {
-        personName: 'John Doe', 
-        itemCollectible: 2500, 
-        collectionStatus: 'Collecting' 
-    },
-    {
-        personName: 'John Doe', 
-        itemCollectible: 2500, 
-        collectionStatus: 'Collecting' 
-    },
-]
-
-/*
-                {
-                    assignedCollectors.map((item, index)=>{
-                        return <MyCollectorList key={index} personName={item.personName} itemCollectible={item.itemCollectible} collectionStatus={item.collectionStatus}/>
-                    })
-                }
-*/
 export default function MyCollectors(){
     const [sendRequest, assignCollector, loading, error,client_user, reseller_user, collector_user, contract] = RestAPI(); 
     const auth = useContext(AuthContext); 
 
 
     useEffect(() => {
-        sendRequest({ 
-            method: 'GET', 
-            url: BASE_URL+"/my-collectors/assigned/" + auth?.user.entityId
-        })
-        console.log(auth?.user.entityId)
-
-    },[] )
-
-    return(
-
+        sendRequest({
+          method: 'GET',
+          url: BASE_URL + "/my-collectors/assigned/" + auth?.user.entityId,
+        });
+      }, []);
+    
+      return (
         <View style={styles.container}>
-            {loading? (
-                <View style={{justifyContent: 'center', alignContent: 'center', alignItems: 'center'}}>
-                    <ActivityIndicator style={{margin: hp(25)}}size="large" />
-                </View>
-            )
-            :(
-                <View style={styles.container}>
-                <Text style={styles.textHeader} >Assigned Collectors</Text>
-                    <FlatList
-                        data={collector_user} // Use contracts instead of client_user
-                        keyExtractor={(contract) => contract.contract_id.toString()} // Adjust keyExtractor
-                        renderItem={({ item: contract }) => (
-                            <MyCollectorList
-                            key={contract.contract_id.toString()} 
-                            personName={contract.username} 
-                            itemCollectible={contract.itemName} 
-                            collectionStatus={contract.dueAmount}                             
-                            />                    
-                        )}
+          {loading ? (
+            <View style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
+              <ActivityIndicator style={{ margin: hp(25) }} size="large" />
+            </View>
+          ) : (
+            <View style={styles.container}>
+              <Text style={styles.textHeader}>Assigned Collectors</Text>
+              {collector_user && Object.keys(collector_user).map((collectorUsername) => (
+                <FlatList
+                  key={collectorUsername}
+                  data={collector_user[collectorUsername]}
+                  keyExtractor={(item) => item.contract_id.toString()}
+                  renderItem={({ item }) => (
+                    <MyCollectorList
+                      key={item.contract_id}
+                      personName= {collectorUsername}
+                      itemCollectible={item.dueAmount}
+                      collectionStatus="Collection"
                     />
-                </View>
-                
-                )}
+                  )}
+                />
+              ))}
+            </View>
+          )}
         </View>
-    );
+      );
 }
 
 const styles = StyleSheet.create({
