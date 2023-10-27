065a9a056c6cef7a09b661a3887c56daa3ba2110
kbdiamondes <kbdiamantes@gmail.com>
2023-10-27 11:31:41 +0800

    coldmorning_1027v1128am
    # Added
    
    ## File
    - MyCollectors
    
    ## Issue
    - Mapping
    - Backend API is not well done.

--- src/Components/Reseller/MyCollectors.tsx
+++ src/Components/Reseller/MyCollectors.tsx
@@ -1,10 +1,13 @@
-import {SafeAreaView, View, Text, StyleSheet, ScrollView} from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet, ScrollView, FlatList, ActivityIndicator} from 'react-native';
 
 
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ScheduledPaymentsList from './Lists/MyCollectorList';
 import MyCollectorList from './Lists/MyCollectorList';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { AuthContext } from '../../Context/AuthContext';
+import { BASE_URL } from '../../../config';
+import { RestAPI } from '../../Services/RestAPI';
 
 const assignedCollectors = [
     {
@@ -28,24 +31,55 @@ const assignedCollectors = [
         collectionStatus: 'Collecting' 
     },
 ]
-export default function MyCollectors(){
-    
 
-    return(
-
-        <SafeAreaView>
-            <ScrollView>
-            <View style={styles.container}>
-                <Text style={styles.textHeader} >Assigned Collectors</Text>
+/*
                 {
                     assignedCollectors.map((item, index)=>{
                         return <MyCollectorList key={index} personName={item.personName} itemCollectible={item.itemCollectible} collectionStatus={item.collectionStatus}/>
                     })
                 }
-            </View>    
-            </ScrollView>     
-        </SafeAreaView>
+*/
+export default function MyCollectors(){
+    const [sendRequest, assignCollector, loading, error,client_user, reseller_user, collector_user, contract] = RestAPI(); 
+    const auth = useContext(AuthContext); 
 
+
+    useEffect(() => {
+        sendRequest({ 
+            method: 'GET', 
+            url: BASE_URL+"/my-collectors/assigned/" + auth?.user.entityId
+        })
+        console.log(auth?.user.entityId)
+
+    },[] )
+
+    return(
+
+        <View style={styles.container}>
+            {loading? (
+                <View style={{justifyContent: 'center', alignContent: 'center', alignItems: 'center'}}>
+                    <ActivityIndicator style={{margin: hp(25)}}size="large" />
+                </View>
+            )
+            :(
+                <View style={styles.container}>
+                <Text style={styles.textHeader} >Assigned Collectors</Text>
+                    <FlatList
+                        data={collector_user} // Use contracts instead of client_user
+                        keyExtractor={(contract) => contract.contract_id.toString()} // Adjust keyExtractor
+                        renderItem={({ item: contract }) => (
+                            <MyCollectorList
+                            key={contract.contract_id.toString()} 
+                            personName={contract.username} 
+                            itemCollectible={contract.itemName} 
+                            collectionStatus={contract.dueAmount}                             
+                            />                    
+                        )}
+                    />
+                </View>
+                
+                )}
+        </View>
     );
 }
 
